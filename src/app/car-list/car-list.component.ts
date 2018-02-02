d872dda2d6a15e272f1b37531e93f9ef40e6d35b
Valentin <valentin.berard42@gmail.com>
2018-02-02 09:45:45 +0100

    test event onclic select a car and save it in selectedCar:Car / favincon changed

--- src/app/car-list/car-list.component.ts
+++ src/app/car-list/car-list.component.ts
@@ -8,16 +8,26 @@ import { Car } from '../model/car';
 })
 export class CarListComponent implements OnInit {
 
+  //Variables
   nomListe = "BroumBroum (voitures)";
   cars: Car[] = [
     { id: 1, model: "207", numberOfPlaces: 5 },
     { id: 2, model: "Golf", numberOfPlaces: 3 },
     { id: 3, model: "Megane", numberOfPlaces: 4 }
   ];
+  selectedCar:Car;
 
+  //
   constructor() { }
 
+  //
   ngOnInit() {
+  }
+
+  //Save this.car when clicOnIt
+  onSelect(car: Car): void {
+    console.log("selected car: ", car);
+    this.selectedCar = car;
   }
 
 }
