b83702878da7b9a19eade4aa41efcee7fab333aa
Valentin <valentin.berard42@gmail.com>
2018-02-05 16:40:59 +0100

    add router from created car to listed car

--- src/app/car-create/car-create.component.ts
+++ src/app/car-create/car-create.component.ts
@@ -3,6 +3,8 @@ import { Car } from '../model/car';
 
 import { Observable } from 'rxjs/Observable';
 import { CarService } from '../car.service';
+import { Router } from '@angular/router';
+import { CarListComponent } from '../car-list/car-list.component';
 
 @Component({
   selector: 'app-car-create',
@@ -14,14 +16,13 @@ export class CarCreateComponent implements OnInit {
   newCar: Car = new Car();
   car: Car;
 
-  constructor( private carService: CarService ) { }
+  constructor(private carService: CarService, private router: Router ) { }
 
   ngOnInit() {
   }
 
   onCreate(): void {
     this.carService.postCar(this.newCar).subscribe(car => this.car);
-
+    this.router.navigateByUrl("car-list");
   }
-  
 }
